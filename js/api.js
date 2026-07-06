637102e15ece01c65a5d5bf6393ce7030b3b874a
Karoll Reis <128089568+hkaroll@users.noreply.github.com>
2026-07-05 23:13:05 -0300

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,72 @@
+const api = {
+  async buscarPensamentos() {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos')
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao salvar pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch (error) {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+  async excluirPensamento(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+        method: "DELETE",
+      })
+    }
+    catch (error) {
+      alert('Erro ao excluir um pensamento')
+      throw error
+    }
+  },
+  
+}
+
+export default api
